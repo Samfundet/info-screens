9bca6f38bd9898d2bb960788af64c2f02f46fdbe
sindrelothe <sindrl@online.no>
2022-03-22 21:53:08 +0100

    Add navbar component

--- infoscreen/templatetags/infoscreen_tags.py
+++ infoscreen/templatetags/infoscreen_tags.py
@@ -1,16 +1,26 @@
 # imports
 from django import template
 from django.conf import settings
+
 # End: imports -----------------------------------------------------------------
 
 register = template.Library()
 
 # https://docs.djangoproject.com/en/3.0/howto/custom-template-tags/
 
-@register.inclusion_tag('infoscreen/components/screen.html')
+
+@register.inclusion_tag("infoscreen/components/screen.html")
 def screen_component(screen, *args, **kwargs):
-    default_classes = ''
+    default_classes = ""
     return {
-        'screen': screen,
-        'classes': kwargs.get('classes', default_classes),
+        "screen": screen,
+        "classes": kwargs.get("classes", default_classes),
+    }
+
+
+@register.inclusion_tag("infoscreen/components/navbar.html")
+def navbar_component(*args, **kwargs):
+    default_classes = ""
+    return {
+        "classes": kwargs.get("classes", default_classes),
     }
