b0bcc49f4b7329bb1b7c4c6ee32ae57cb039ecff
sindrelothe <sindrl@online.no>
2022-04-26 19:35:25 +0200

    added trailing comma to urls.py

--- root/urls.py
+++ root/urls.py
@@ -20,7 +20,10 @@ from django.urls import path, include
 from django.contrib import admin
 from django.conf.urls.static import static
 
-urlpatterns = [path('admin/', admin.site.urls), path('', include('infoscreen.urls'))]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('infoscreen.urls')),
+]
 
 urlpatterns += static(prefix=settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
